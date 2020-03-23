7f22a4a8b3c06ca67e8e5cad0d1d2ec67764164c
Hedi Ben Mustapha <hedi.benmustapha@msb.tn>
2020-03-23 16:11:34 +0100

    add percentage sign to Freq

--- R/MainBar.R
+++ R/MainBar.R
@@ -126,11 +126,9 @@ Make_main_bar <- function(Main_bar_data, Q, show_num, ratios, customQ, number_an
                     + theme(panel.background = element_rect(fill = "white"),
                             plot.margin = unit(c(0.5,0.5,bottom_margin,0.5), "lines"), panel.border = element_blank(),
                             axis.title.y = element_text(vjust = -0.8, size = 8.3*y_axis_title_scale), axis.text.y = element_text(vjust=0.3,
-                                                                                                            size=7*y_axis_tick_label_scale)))
-  if((show_num == "yes") || (show_num == "Yes")){
-    Main_bar_plot <- (Main_bar_plot + geom_text(aes_string(label = "freq"), size = 2.2*intersection_size_number_scale, vjust = -1,
-                                                angle = number_angles, colour = Main_bar_data$color))
-  }
+                                                                                                            size=7*y_axis_tick_label_scale))
+                    + geom_text(label = paste0(Main_bar_data$freq, "%"), size = 2.2*intersection_size_number_scale, vjust = -1,
+                                angle = number_angles, colour = Main_bar_data$color))
   bInterDat <- NULL
   pInterDat <- NULL
   bCustomDat <- NULL
